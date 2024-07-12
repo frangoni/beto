a1d54721c2d96fb1aa87a7fdcaf9bfa66308e80e
frangoni <fran.goni96@gmail.com>
2024-07-11 21:57:44 -0300

    palette color changer

--- src/components/palette/PaletteSelector.tsx
+++ src/components/palette/PaletteSelector.tsx
@@ -1,23 +1,61 @@
+import { useState, useEffect } from 'react';
 import { styled } from 'styled-components';
 import { PALETTES } from '../../settings/theme';
 import Brush from './icons/brush';
 import { IconStyle } from '../navbar/Navbar';
+import gsap from 'gsap';
 
 interface PaletteSelectorProps {
 	setPaletteIndex: (index: number) => void;
 }
 
 export default function PaletteSelector({ setPaletteIndex }: PaletteSelectorProps) {
-	function changePalette() {
-		//@ts-ignore
-		setPaletteIndex(prev => (prev + 1) % PALETTES.length);
+	const [showPalettes, setShowPalettes] = useState(false);
+
+	function togglePaletteIcons() {
+		setShowPalettes(prev => !prev);
 	}
+
+	function changePalette(index: number) {
+		setPaletteIndex(index);
+	}
+
+	useEffect(() => {
+		if (showPalettes) {
+			gsap.to('.palette-icon', {
+				opacity: 1,
+				y: 0,
+				stagger: 0.1,
+				duration: 0.3,
+				ease: 'power1.out',
+			});
+		} else {
+			gsap.to('.palette-icon', {
+				opacity: 0,
+				y: 20,
+				duration: 0.3,
+				ease: 'power1.in',
+				stagger: 0.1,
+			});
+		}
+	}, [showPalettes]);
 
 	return (
 		<>
-			<FloatingIcon onClick={changePalette}>
+			<FloatingIcon onClick={togglePaletteIcons}>
 				<Brush />
 			</FloatingIcon>
+			{PALETTES.map((palette, index) => (
+				<PaletteIcon
+					index={index}
+					key={index}
+					className='palette-icon'
+					onClick={() => changePalette(index)}
+					style={{ opacity: 0 }}
+				>
+					<ColorIcon style={{ background: `linear-gradient(45deg, ${palette[0]} 50%, ${palette[1]} 50%)` }} />
+				</PaletteIcon>
+			))}
 		</>
 	);
 }
@@ -55,4 +93,37 @@ const FloatingIcon = styled.div`
 			height: 2rem;
 		}
 	}
+`;
+
+const PaletteIcon = styled.div<{ index: number }>`
+	position: fixed;
+	left: 3.3rem;
+	bottom: ${({ index }) => `calc(8rem + ${index} * 2.5rem)`};
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	cursor: pointer;
+	border-radius: 50%;
+	overflow: hidden;
+	width: 2rem;
+	height: 2rem;
+	background: ${({ theme }) => theme.secondaryBackground};
+	border: 2px solid ${({ theme }) => theme.card.border};
+	z-index: 100;
+	transition: scale 0.3s;
+
+	&:hover {
+		scale: 1.1 !important;
+	}
+
+	@media screen and (max-width: 768px) {
+		left: ${({ index }) => `calc(4rem + ${index} * 2.5rem)`};
+		bottom: 1.5rem;
+	}
+`;
+
+const ColorIcon = styled.div`
+	width: 100%;
+	height: 100%;
+	border-radius: 50%;
 `;
